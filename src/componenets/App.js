2a77ce26bd5796b412564a02d27f43b5beafa28e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-03 20:33:33 +0200

    add list and box

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import CommentBox from './CommentBox'
+import CommentList from './CommentList'
+
 
 export default () => {
-  return <div>Im the App component</div>
+  return (<div>
+    <CommentBox />
+    <CommentList />
+    </div>)
 }
