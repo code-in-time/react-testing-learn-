8f0078fb8c65c8044c0acc89ec4dd12a3a6a1a5e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-05 04:53:15 +0200

    Add first enzyme test

--- src/componenets/__tests__/App.test.js
+++ src/componenets/__tests__/App.test.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
+import CommentBox from '../CommentBox'
+import {shallow} from 'enzyme'
 
-it('shows a comment block', () => {
+
+
+it('shows a comment block no enzyme but t est for text', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   console.log(div.innerHTML)
   expect(div.innerHTML).toContain('Comment box')
   ReactDOM.unmountComponentAtNode(div)
+  // expect
+})
+
+it('shows a comment block using enzyme', () => {
+  const wrapper = shallow(<App />);
+  console.log('wrapper.find(CommentBox).length', wrapper.find(CommentBox).length)
+  expect(wrapper.find(CommentBox).length).toBe(1)
   // expect
 })
