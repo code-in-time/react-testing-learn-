f78f4bf714a839392c3cc47e33a09f8778d7e90b
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-05 04:59:04 +0200

    CommentList test

--- src/componenets/__tests__/App.test.js
+++ src/componenets/__tests__/App.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
 import CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
 import {shallow} from 'enzyme'
 
 
@@ -9,15 +10,17 @@ import {shallow} from 'enzyme'
 it('shows a comment block no enzyme but t est for text', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
-  console.log(div.innerHTML)
+  // console.log(div.innerHTML)
   expect(div.innerHTML).toContain('Comment box')
   ReactDOM.unmountComponentAtNode(div)
-  // expect
 })
 
 it('shows a comment block using enzyme', () => {
   const wrapper = shallow(<App />);
-  console.log('wrapper.find(CommentBox).length', wrapper.find(CommentBox).length)
   expect(wrapper.find(CommentBox).length).toBe(1)
-  // expect
+})
+
+it('shows a comment list using enzyme', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toBe(1)
 })
