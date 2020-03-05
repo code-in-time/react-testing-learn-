fd87489f26e2ef6f02f0b4b3fa0c44083b0bfe28
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-05 05:17:03 +0200

    beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,7 +5,11 @@ import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 import {shallow} from 'enzyme'
 
+let wrapper;
 
+beforeEach(() => {
+  wrapper = shallow(<App />);
+})
 
 it('shows a comment block no enzyme but t est for text', () => {
   const div = document.createElement('div')
@@ -16,11 +20,11 @@ it('shows a comment block no enzyme but t est for text', () => {
 })
 
 it('shows a comment block using enzyme', () => {
-  const wrapper = shallow(<App />);
+  // const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toBe(1)
 })
 
 it('shows a comment list using enzyme', () => {
-  const wrapper = shallow(<App />);
+  // const wrapper = shallow(<App />);
   expect(wrapper.find(CommentList).length).toBe(1)
 })
