099cbe99d7c4dfa0930fd324cf1b01b59a4c0058
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-08 20:39:06 +0200

    nested describe

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -14,18 +14,25 @@ it('has a text area and a button', () => {
   expect(wrapper.find("button").length).toEqual(1)
 })
 
-it('can type in the text area', () => {
-  wrapper.find("textarea").simulate('change', { target: { value: 'new comment' } });
-  wrapper.update()
-  expect(wrapper.find("textarea").prop('value')).toEqual('new comment')
-})
 
-it('click button and the text area is clear', () => {
-  wrapper.find("textarea").simulate('change', { target: { value: 'new comment' } });
-  wrapper.update()
-  wrapper.find("form").simulate('submit');
-  wrapper.update()
-  expect(wrapper.find("textarea").prop('value')).toEqual('')
+describe(' the text area', () => {
+
+  beforeEach(() => {
+    wrapper.find("textarea").simulate('change', { target: { value: 'new comment' } });
+    wrapper.update()
+  })
+
+  it('can type in the text area', () => {
+    expect(wrapper.find("textarea").prop('value')).toEqual('new comment')
+  })
+
+  it('click button and the text area is clear', () => {
+    expect(wrapper.find("textarea").prop('value')).toEqual('new comment')
+    wrapper.find("form").simulate('submit');
+    wrapper.update()
+    expect(wrapper.find("textarea").prop('value')).toEqual('')
+  })
+
 })
 
 afterEach(() => {
