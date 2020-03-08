fb9a59eec9a60896e7900f2c244a5ffcb22b5316
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-08 20:53:49 +0200

    redux setup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Learning react testing
 
 ### https://www.udemy.com/course/react-redux-tutorial/learn/lecture/10466946#overview
+### https://www.udemy.com/course/react-redux-tutorial/learn/lecture/10467032#overview
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import store from './Reducers'
+
 import App from './components/App'
 
 
 ReactDom.render(
-  <App/>
+  <Provider store={createStore(store, {})}><App/></Provider>
   , document.getElementById('root')
 )
