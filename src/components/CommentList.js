bede712ae9ad0046e9b7bf4442d4507ea572d004
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-13 05:51:34 +0200

    save

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
+// import ReactDOM from 'react-dom'
+import { connect } from 'react-redux'
 
-export default () => {
-  return <div>Comment list</div>
+class CommentList extends Component {
+
+  renderComments() {
+    this.props.comments.map(comment => {
+      return <li ket={comment}>{comment}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    )
+  }
+
 }
+
+const mapStateToProps = (state) => ({
+  comments: state.comments
+})
+
+export default connect(mapStateToProps)(CommentList)
