c8069995303001147e343527b2e2da22ccfc27f8
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-17 06:35:08 +0200

    integration tests comments

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Root from '../Root'
+import App from '../components/App'
+
+it('can fetch a list of comments and display them', () => {
+  // Attempt to render app
+
+  // Find fetch comments button and click it
+
+  // Expect a list of 500 comments
+})
