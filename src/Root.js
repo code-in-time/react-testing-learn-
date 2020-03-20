7c7254923be197f9724cdfd2a0f0dc7d2b405c88
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-03-20 05:16:03 +0200

    Make test pass

--- src/Root.js
+++ src/Root.js
@@ -11,11 +11,11 @@ const store = (initialState) => createStore(
     applyMiddleware(
       reduxPromise
     ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
-export default ({children, initialState ={}}) => (
+export default ({children, initialState = {}}) => (
   <Provider store={store(initialState)}>
     {children}
   </Provider>
